b4cb443e8a3344753cbeb4e19faa3a01eaa178a4
Егор Комаров <geoolekom@gmail.com>
2019-11-21 01:53:00 +0300

    Instant paste and different buttons

--- src/nautilus-copypaste-images.py
+++ src/nautilus-copypaste-images.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import datetime
 import gi
 try:
     gi.require_version('Gtk', '3.0')
@@ -26,12 +27,11 @@ try:
 except Exception as e:
     print(e)
     exit(-1)
+
 import os
-from threading import Thread
 from urllib import unquote_plus
 from gi.repository import GObject
 from gi.repository import Gtk
-from gi.repository import GLib
 from gi.repository import Nautilus as FileManager
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
@@ -41,25 +41,9 @@ ICON = 'nautilus-copypaste-images'
 VERSION = '0.2.0-0extras16.04.1'
 
 
-def get_suported_extensions():
-    temp = []
-    for aformat in GdkPixbuf.Pixbuf.get_formats():
-        tmp_extensions = aformat.get_extensions()
-        temp.extend(tmp_extensions)
-    extensions = []
-    for extension in temp:
-        extensions.append('.'+extension)
-    return extensions
-
-EXTENSIONS_FROM = get_suported_extensions()
-SAVETO = ['.png', '.jpg', '.tif', '.ico', '.bmp']
-_ = str
-
-
 def get_files(files_in):
     files = []
     for file_in in files_in:
-        print(file_in)
         file_in = unquote_plus(file_in.get_uri()[7:])
         if os.path.isfile(file_in):
             files.append(file_in)
@@ -75,111 +59,39 @@ class CopyPasteImagesMenuProvider(GObject.GObject, FileManager.MenuProvider):
         method"""
         atom = Gdk.atom_intern('CLIPBOARD', True)
         self.clipboard = Gtk.Clipboard.get(atom)
+        super(CopyPasteImagesMenuProvider, self).__init__()
 
-    def all_files_are_images(self, items):
-        for item in items:
-            fileName, fileExtension = os.path.splitext(item.get_uri()[7:])
-            if fileExtension.lower() not in EXTENSIONS_FROM:
-                return False
-        return True
+    def get_background_items(self, window, folder):
+        paste_item = FileManager.MenuItem(name='CopyPasteImagesMenuProvider::Gtk-cpim-paste-image',
+                                          label='Вставить как изображение')
+        location = folder.get_location()
+        paste_item.connect('activate', self.paste_image, location.get_path())
+        return paste_item,
 
-    def get_file_items(self, window, sel_items):
-        """
-        Adds the 'Replace in Filenames' menu item to the FileManager
-        right-click menu, connects its 'activate' signal to the 'run' method
-        passing the selected Directory/File
-        """
-        # if not self.all_files_are_images(sel_items):
-        #    return
-        top_menuitem = FileManager.MenuItem(
-            name='CopyPasteImagesMenuProvider::Gtk-image-tools',
-            label=_('CopyPaste Images'),
-            tip=_('Tools to copy and paste images'),
-            icon='Gtk-find-and-replace')
-        submenu = FileManager.Menu()
-        top_menuitem.set_submenu(submenu)
-        if self.all_files_are_images(sel_items):
-            sub_menuitem01 = FileManager.MenuItem(
-                name='CopyPasteImagesMenuProvider::Gtk-copy-image',
-                label='Copy image',
-                tip='Copy image to clipboard')
-            sub_menuitem01.connect('activate', self.copy_image, sel_items,
-                                   window)
-            submenu.append_item(sub_menuitem01)
-        if self.clipboard.wait_is_image_available():
-            sub_menuitem02 = FileManager.MenuItem(
-                name='CopyPasteImagesMenuProvider::Gtk-paste-image',
-                label='Paste image',
-                tip='Paste image from the clipboard')
-            sub_menuitem02.connect('activate', self.paste_image, window)
-            submenu.append_item(sub_menuitem02)
-        sub_menuitem_98 = FileManager.MenuItem(
-            name='CopyPasteImagesMenuProvider::Gtk-none',
-            label=Gtk.SeparatorMenuItem())
-        submenu.append_item(sub_menuitem_98)
-        sub_menuitem_99 = FileManager.MenuItem(
-            name='CopyPasteImagesMenuProvider::Gtk-document-converter-99',
-            label=_('About'),
-            tip=_('About'),
-            icon='Gtk-find-and-replace')
-        sub_menuitem_99.connect('activate', self.about)
-        submenu.append_item(sub_menuitem_99)
-        return top_menuitem,
-
-    def copy_image(self, menu, files, window):
-        files = get_files(files)
+    def get_file_items(self, window, items):
+        files = get_files(items)
+        copy_item = FileManager.MenuItem(name='CopyPasteImagesMenuProvider::Gtk-cpim-copy-image',
+                                         label='Копировать как изображение', sensitive=False)
         if len(files) > 0:
-            afile = files[0]
-            if not os.path.exists(afile):
-                md = Gtk.MessageDialog(parent=window,
-                                       flags=Gtk.DialogFlags.MODAL |
-                                       Gtk.DialogFlags.DESTROY_WITH_PARENT,
-                                       type=Gtk.MessageType.ERROR,
-                                       buttons=Gtk.ButtonsType.OK,
-                                       message_format='No existe el archivo')
-                md.run()
-                exit(-1)
-            pixbuf = GdkPixbuf.Pixbuf.new_from_file(afile)
+            chosen_file = files[0]
+            info, _, _ = GdkPixbuf.Pixbuf.get_file_info(chosen_file)
+            if info is not None:
+                copy_item.set_property('sensitive', True)
+                copy_item.connect('activate', self.copy_image, chosen_file)
+        return copy_item,
+
+    def paste_image(self, menu, dirname):
+        pixbuf = self.clipboard.wait_for_image()
+        current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        filename = os.path.join(dirname, 'from_clipboard_{0}.png'.format(current_time))
+        if pixbuf is not None:
+            pixbuf.savev(filename, 'png', (), ())
+
+    def copy_image(self, menu, file):
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file(file)
+        if pixbuf:
             self.clipboard.set_image(pixbuf)
             self.clipboard.store()
-
-    def paste_image(self, menu, window):
-        pixbuf = self.clipboard.wait_for_image()
-        if pixbuf is not None:
-            dialog = Gtk.FileChooserDialog('Guardar la imagen del\
-                                            portapapeles como ...',
-                                           window,
-                                           Gtk.FileChooserAction.SAVE,
-                                           (Gtk.STOCK_CANCEL,
-                                            Gtk.ResponseType.CANCEL,
-                                            Gtk.STOCK_OPEN,
-                                            Gtk.ResponseType.OK))
-            dialog.set_default_response(Gtk.ResponseType.OK)
-            dialog.set_do_overwrite_confirmation(True)
-            dialog.set_current_folder(os.getenv('HOME'))
-            dialog.set_filename('from_copy_paste.png')
-            filter = Gtk.FileFilter()
-            filter.set_name('Imagenes')
-            filter.add_mime_type('image/png')
-            filter.add_mime_type('image/jpeg')
-            filter.add_mime_type('image/tiff')
-            filter.add_mime_type('image/ico')
-            filter.add_mime_type('image/bmp')
-            filter.add_pattern('*.png')
-            filter.add_pattern('*.jpg')
-            filter.add_pattern('*.tif')
-            filter.add_pattern('*.ico')
-            filter.add_pattern('*.bmp')
-            dialog.add_filter(filter)
-            response = dialog.run()
-            if response == Gtk.ResponseType.OK:
-                filename = dialog.get_filename()
-                dialog.destroy()
-                if filename[-4:].lower() in SAVETO:
-                    pixbuf.savev(filename, filename[-3:].lower(), (), ())
-                else:
-                    filename = filename + '.png'
-                    pixbuf.savev(filename, 'png', (), ())
 
     def about(self, widget, window):
         ad = Gtk.AboutDialog(parent=window)
@@ -210,6 +122,7 @@ this program. If not, see <http://www.gnu.org/licenses/>.
         ad.set_logo_icon_name(APPNAME)
         ad.run()
         ad.destroy()
+
 
 if __name__ == '__main__':
     '''
